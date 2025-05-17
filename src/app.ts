60e9380a8ae15d2473f8870e4ea127e4423a7325
morshed0099 <mdgolammorshed0099@gmail.com>
2025-05-17 22:44:00 +0600

    server created and database created also create pisma customer model

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+const app: Application = express();
+app.use(cors());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send({
+    message: "tamim agro",
+  });
+});
+
+export default app;
